763a40371411f1f90a79c783a8395cfcffdd986f
Andre Luiz <97361918+AndreLuizBoanergers@users.noreply.github.com>
2024-10-05 11:59:13 -0300

    Create classeJFA_prefacio.php

---
+++ classeJFA_prefacio.php
@@ -0,0 +1,30 @@
+<?php
+//JOAO FERREIRA DE ALMEIDA ATUALIZADA E CORRIGIDA
+class JfaARC
+{
+	public function __contruct($jsonJFA){
+		$jsonJFAContent = file_get_contents($jsonJFA);
+		$this->data = json_decode($jsonJFAContent, true);
+
+		if($this->data === null){
+			throw new Exception("Erro ao decodificar o JSON");
+		}
+	}
+
+	public function getPrefacio(){
+		foreach ($this->data as $livro) {
+			if(isset($livro['abbrev']) && isset($livro['chapters'])){
+				$abbrev = $livro['abbrev'];
+				$totalCapitulos = count($livro['chapters']);
+
+				$resumo[] = [
+                    'abreviadura' => $abbrev,
+                    "total_capitulos" => $totalCapitulos
+				];
+
+			}
+		}
+
+		return json_encode($resumo, JSON_PRETTY_PRINT);
+	}
+}
